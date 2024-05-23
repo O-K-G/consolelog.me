c3084d4e7fc190c41bde7f3543e9241a22019b3a
O. K. G. <inariwind@gmail.com>
2024-05-23 18:46:13 -0400

    chore: disable in production

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,9 @@ import Test from '@components/Test';
 // TODO: Remove comments.
 
 export default function Home() {
+  if (process.env.NODE_ENV !== 'development') {
+    return <div>TBD</div>;
+  }
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <Test />
