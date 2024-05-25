da2c8db9cbc8d4052035f9046a71bf367132c75e
O. K. G. <inariwind@gmail.com>
2024-05-25 17:30:15 -0400

    fix: cleanup

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,8 +1,7 @@
 'use client';
 
 import { usePathname } from 'next/navigation';
-import { useEffect, useRef, useState, type ReactNode } from 'react';
-import useVpSize from '@hooks/useVpSize';
+import { useEffect, useRef, type ReactNode } from 'react';
 
 interface SectionProps {
   children: ReactNode;
@@ -33,31 +32,12 @@ export default function Section({
     }
   }, [currentSection, pathname]);
 
-  const { isTailwindMobile } = useVpSize();
-  const [fileType, setFileType] = useState<null | 'svg' | 'webp'>(null);
-
-  /** Ensures that large screens will get the SVG backgrounds and not the WEBP ones,
-   *  event if they're more resource intensive.
-   *  The screen size check is done only initially, changing the screen size
-   *  will not cause the other file types to download, as it's a redundant network use
-   *  for most cases.
-   */
-
-  // useEffect(() => {
-  //   if (!fileType) {
-  //     if (isTailwindMobile) {
-  //       return setFileType('webp');
-  //     }
-  //     return setFileType('svg');
-  //   }
-  // }, [currentSection, isTailwindMobile, fileType]);
-
   const backgroundImage = {
-    about: 'bg-about-background-webp',
-    contact: 'bg-contact-background-webp',
-    skills: 'bg-skills-background-webp',
-    projects: 'bg-projects-background-webp',
-    experience: 'bg-experience-background-webp',
+    about: 'bg-about-background',
+    contact: 'bg-contact-background',
+    skills: 'bg-skills-background',
+    projects: 'bg-projects-background',
+    experience: 'bg-experience-background',
   };
 
   const currentBackgroundImage = backgroundImage[currentSection] || '';
