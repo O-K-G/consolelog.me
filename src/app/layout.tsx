0bec20153cd7cfebe2588e398abc4a2388e0d785
O. K. G. <inariwind@gmail.com>
2024-05-28 22:18:44 -0400

    fix: move fonts to public

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,19 @@
 import type { Metadata, Viewport } from 'next';
 
 import './globals.css';
-import { JUST_IN_THE_FIRESTORM_REGULAR_FONT } from '@constants/fonts';
+import localFont from 'next/font/local';
+
+/** https://www.fontspace.com/just-in-the-firestorm-font-f31017 */
+export const JUST_IN_THE_FIRESTORM_REGULAR_FONT = localFont({
+  src: [
+    {
+      path: '../../public/fonts/justInTheFirestormRegular-z291.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-just-in-the-firestorm',
+});
 
 // TODO: Set metadada.
 
