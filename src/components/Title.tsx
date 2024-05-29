8a12c5f97b65641b362fcdaf8e6de1ae6e0d7e3a
O. K. G. <inariwind@gmail.com>
2024-05-28 20:53:18 -0400

    feat: glow - stage 2

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,18 +1,49 @@
 import type { TitleProps } from '@constants/interfaces';
 
+interface CharacterProps {
+  character: string | number;
+  widthClassName: string;
+}
+
+function Character({ character, widthClassName }: CharacterProps) {
+  return (
+    <div className={`relative ${widthClassName}`}>
+      <div
+        aria-hidden
+        className='select-none pointer-events-none title-character blur-sm scale-[1.05]'
+      >
+        {character}
+      </div>
+      <div aria-hidden className='title-character'>
+        {character}
+      </div>
+    </div>
+  );
+}
+
 export default function Title({
   component: Component = 'h2',
   label,
 }: TitleProps) {
+  const charactersArr = label.split('').map((str, index) => ({
+    id: index,
+    str,
+  }));
+
   return (
-    <div className='relative'>
-      <div
-        aria-hidden
-        className='select-none pointer-events-none title-text blur-sm title-text-stroke scale-[1.05]'
-      >
-        A
-      </div>
-      <Component className='title-text title-text-stroke'>A</Component>
+    <div className='flex items-center justify-center'>
+      <Component className='sr-only'>{label}</Component>
+      {charactersArr.map(({ id, str }) => {
+        const isI = str === 'i' || str === 'I';
+
+        return (
+          <Character
+            widthClassName={isI ? 'w-8' : 'w-16'}
+            key={`character-${label}-${str}-${id}`}
+            character={str}
+          />
+        );
+      })}
     </div>
   );
 }
