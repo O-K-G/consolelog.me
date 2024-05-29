8e5708d85a69cd1056efc6516a4649a863fde47a
O. K. G. <inariwind@gmail.com>
2024-05-28 22:43:08 -0400

    fix: import font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,13 +5,7 @@ import localFont from 'next/font/local';
 
 /** https://www.fontspace.com/just-in-the-firestorm-font-f31017 */
 export const JUST_IN_THE_FIRESTORM_REGULAR_FONT = localFont({
-  src: [
-    {
-      path: 'justInTheFirestormRegular-z291.ttf',
-      weight: '400',
-      style: 'normal',
-    },
-  ],
+  src: '../../public/fonts/JustInTheFirestormRegular-z291.ttf',
   variable: '--font-just-in-the-firestorm',
 });
 
