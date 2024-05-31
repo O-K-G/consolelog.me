a320846d34a98a694c09107a4c12fafbcce96491
O. K. G. <inariwind@gmail.com>
2024-05-30 20:47:53 -0400

    fix: firefox for android visual updates

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -13,12 +13,16 @@ function TitleBorder({ leftLabel, rightLabel, className }: TitleBorderProps) {
     <div
       className={`text-title-purple uppercase text-base lg:text-xl font-star-date-81316 whitespace-nowrap center-elements gap-2 w-full ${className}`}
     >
-      {leftLabel && <span className='leading-none pt-0.5'>{leftLabel}</span>}
-      <div className='center-elements relative w-full h-3 lg:h-3.5'>
+      {leftLabel && (
+        <span className='leading-none pt-[0.06rem]'>{leftLabel}</span>
+      )}
+      <div className='center-elements relative w-full h-[0.64rem] lg:h-3.5'>
         <div className={`${titleBorderClassName} blur-sm border-[1.5px]`} />
         <div className={titleBorderClassName} />
       </div>
-      {rightLabel && <span className='leading-none pt-0.5'>{rightLabel}</span>}
+      {rightLabel && (
+        <span className='leading-none pt-[0.06rem]'>{rightLabel}</span>
+      )}
     </div>
   );
 }
@@ -27,7 +31,7 @@ function Character({ character, widthClassName }: CharacterProps) {
   // TODO: Check if aria-hidden should move to the parent div instead of the two children.
 
   return (
-    <div className={`relative min-h-9 lg:min-h-16 lowercase ${widthClassName}`}>
+    <div className={`relative min-h-8 lg:min-h-16 lowercase ${widthClassName}`}>
       <div className='select-none pointer-events-none title-character blur-[2px] lg:blur-sm scale-[1.05]'>
         <div aria-hidden className='center-elements size-full'>
           {character}
@@ -67,7 +71,7 @@ export default function Title({
 
           return (
             <Character
-              widthClassName={isI ? 'w-3 lg:w-4' : 'w-5 lg:w-8'}
+              widthClassName={isI ? 'w-3 lg:w-4' : 'w-4 lg:w-8'}
               key={`character-${label}-${str}-${id}`}
               character={str}
             />
