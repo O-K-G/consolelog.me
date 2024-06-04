c12549f0d18d9d54039db50bf4a67ff6662727c5
O. K. G. <inariwind@gmail.com>
2024-06-04 15:53:16 -0400

    refactor: attr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <main className='min-h-[100svh] lg:min-h-[100dvh] overflow-hidden'>
       <Loader />
-      {/* <div className='fixed top-40 left-0 text-xs text-white z-10'>
+      <div className='fixed top-40 left-0 text-xs text-white z-10'>
         TODO: Move this attribution elsewhere. Design based on Jayendra
         Awasthi&apos;s &apos;Space themed portfolio&apos; free community Figma
         template at&nbsp;
@@ -32,7 +32,7 @@ export default function Home() {
         </a>
         &nbsp; license. Changes were made to the material from the original
         version.
-      </div> */}
+      </div>
       <AppContextComponent>
         <About />
         <Contact />
