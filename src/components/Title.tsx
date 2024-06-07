36733763a3dd5231927363c928cc96ab03cc3984
O. K. G. <inariwind@gmail.com>
2024-06-07 19:54:16 -0400

    fix: stage 1

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -18,7 +18,7 @@ interface BorderProps {
 function Border({ top, bottom, label }: BorderProps) {
   return (
     <div
-      className={`absolute left-0 w-full h-3 lg:h-4 my-2 ${
+      className={`absolute left-0 w-full h-2.5 sm:h-3 md:h-3.5 lg:h-4 xl:h-4.5 2xl:h-5 my-2 ${
         top ? 'top-0' : ''
       } ${bottom ? 'bottom-0' : ''}`}
     >
@@ -28,7 +28,7 @@ function Border({ top, bottom, label }: BorderProps) {
         }`}
       >
         <div className='relative size-full'>
-          <div className='border-2 border-title-purple size-full before:border-2 before:absolute before:top-0 before:-left-[0.05rem] before:bottom-0 before:right-0 before:m-auto before:border-title-purple before:h-[110%] before:w-[101%] lg:before:w-[100.5%] before:blur-[0.125rem]' />
+          <div className='border sm:border-2 border-title-purple size-full before:border before:sm:border-2 before:absolute before:top-0 before:-left-[0.05rem] before:bottom-0 before:right-0 before:m-auto before:border-title-purple before:h-[110%] before:w-[101%] lg:before:w-[100.5%] before:blur-[0.125rem]' />
         </div>
         {label && (
           <span className='text-base z-10 lg:text-[1.5rem] lg:leading-7 pt-[0.120rem] font-star-date-81316 uppercase whitespace-nowrap'>
@@ -89,7 +89,7 @@ export default function Title({
   isButton,
   label,
   labelGlowText,
-  className = 'w-full max-w-[18rem] sm:max-w-[21rem] h-20 lg:h-36 lg:max-w-[32rem]',
+  className = 'w-fit h-20 sm:h-24 md:h-[6.5rem] lg:h-28 xl:h-32 2xl:h-36',
   topLabel,
   bottomLabel,
   border = false,
@@ -103,12 +103,12 @@ export default function Title({
     ? 'title-text-stroke-purple'
     : 'title-text-stroke-white';
 
-  const sharedClassName = `uppercase before:uppercase before:absolute before:top-0 before:bottom-0 before:left-0 before:right-0 before:m-auto before:pointer-events-none before:select-none before:size-full before:center-elements before:flex-wrap before:blur-[0.09rem] lg:before:blur-[0.125rem] before:text-transparent center-elements flex-wrap text-transparent size-full before:title-text-stroke-purple ${componentClassName} ${labelGlowText}`;
+  const sharedClassName = `tracking-[0.2em] sm:tracking-normal uppercase before:uppercase before:absolute before:top-0 before:bottom-0 before:left-0 before:right-0 before:m-auto before:pointer-events-none before:select-none before:size-full before:center-elements before:flex-wrap before:blur-[0.09rem] lg:before:blur-[0.125rem] before:text-transparent center-elements flex-wrap text-transparent size-full before:title-text-stroke-purple ${componentClassName} ${labelGlowText}`;
 
   return (
     <div
       data-open={open}
-      className={`shrink-0 font-just-in-the-firestorm text-xl sm:text-2xl lg:text-4xl relative center-elements ${containerClassName} ${className}`}
+      className={`shrink-0 font-just-in-the-firestorm text-base sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl relative center-elements ${containerClassName} ${className}`}
     >
       {isButton && <AboutTargetIcon open={!!open} />}
       {border && <Border top label={topLabel} />}
