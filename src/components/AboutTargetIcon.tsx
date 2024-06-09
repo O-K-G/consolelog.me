bc8128f2b0023ca4c4b5c9aa4b1a65d200c691b1
O. K. G. <inariwind@gmail.com>
2024-06-09 17:53:43 -0400

    refactor: remove transition event listener

--- src/components/AboutTargetIcon.tsx
+++ src/components/AboutTargetIcon.tsx
@@ -7,12 +7,12 @@ function SVG90Degrees({ className, pathClassName }: SVG90DegreesProps) {
   return (
     <svg
       viewBox='0 0 64 57'
-      className={`standard-transition w-10 sm:w-16 fill-none absolute -z-10 ${className}`}
+      className={`transition-all ease-in-out duration-300 w-10 sm:w-16 fill-none absolute -z-10 ${className}`}
       xmlns='http://www.w3.org/2000/svg'
     >
       <path
         d='M1 1V55.4688H10.7351V10.7073H62.3854V1H1Z'
-        className={`standard-transition stroke-1 sm:stroke-2 ${pathClassName}`}
+        className={`transition-all ease-in-out duration-300 stroke-1 sm:stroke-2 ${pathClassName}`}
       />
     </svg>
   );
@@ -30,11 +30,15 @@ export default function AboutTargetIcon({
     <>
       <SVG90Degrees
         className={`${!open ? '' : 'opacity-0'} ${className}`}
-        pathClassName='stroke-title-purple blur-[0.125rem]'
+        pathClassName='stroke-title-purple blur-[0.125rem] group-hover:stroke-title-orange group-active:stroke-title-red'
       />
       <SVG90Degrees
         className={className}
-        pathClassName={`${!open ? 'stroke-white' : 'stroke-title-purple'}`}
+        pathClassName={`${
+          !open
+            ? 'stroke-white group-hover:stroke-title-orange group-active:stroke-title-red'
+            : 'stroke-title-purple group-hover:stroke-title-orange group-active:stroke-title-red'
+        }`}
       />
     </>
   );
