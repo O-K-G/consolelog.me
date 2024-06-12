77f769847b1ab4bd05fa88d2bfee7df965ba34d8
O. K. G. <inariwind@gmail.com>
2024-06-11 21:07:22 -0400

    fix: button not read

--- src/components/shared/AppContext.tsx
+++ src/components/shared/AppContext.tsx
@@ -4,28 +4,34 @@ import { type AppContextComponentProps } from '@constants/interfaces';
 import {
   type Dispatch,
   type SetStateAction,
+  type MutableRefObject,
   createContext,
   useState,
   useMemo,
+  useRef,
 } from 'react';
 
 export const AppContext = createContext({
   currentTopSection: 'about',
   onChange: () => null,
+  contactSectionRef: { current: null },
 } as {
   currentTopSection: string;
   onChange: Dispatch<SetStateAction<string>>;
+  contactSectionRef: MutableRefObject<HTMLElement | null>;
 });
 
 export default function AppContextComponent({
   children,
 }: AppContextComponentProps) {
   const [currentTopSection, setCurrentTopSection] = useState('about');
+  const contactSectionRef = useRef(null);
 
   const AppContextData = useMemo(
     () => ({
       currentTopSection,
       onChange: setCurrentTopSection,
+      contactSectionRef,
     }),
     [currentTopSection]
   );
