1a07d69a1b2c94c376ecb49176852c930aa67cb8
O. K. G. <inariwind@gmail.com>
2024-06-13 21:52:48 -0400

    refactor: minor refactor

--- src/utils/handleSubmit.tsx
+++ src/utils/handleSubmit.tsx
@@ -3,13 +3,14 @@
 import formValidation from '@utils/formValidation';
 
 export async function handleSubmit(formData: FormData) {
+  const dir = formData.get('dir') as string;
   const email = formData.get('email') as string;
   const subject = formData.get('subject') as string;
   const content = formData.get('content') as string;
 
   const { isValidated } = formValidation({ email, subject, content });
 
-  console.log('server', formData);
+  console.log('server', isValidated, dir);
   // await fetch('http://testteefdfsgsgfvwerfsttest.com').catch((err) => {
   //   console.log(err);
   // });
