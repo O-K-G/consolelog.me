25f3c32de3e58daa811abbbf906e70ea15caf3c8
O. K. G. <inariwind@gmail.com>
2024-06-15 13:00:43 -0400

    feat: visual updates

--- src/components/DialogBackdrop.tsx
+++ src/components/DialogBackdrop.tsx
@@ -59,7 +59,7 @@ export default function DialogBackdrop({
             onKeyDown={(e) => e.stopPropagation()}
             role='textbox'
             tabIndex={-1}
-            className='lg:cursor-default relative w-full h-[50svh] md:w-[50svw] md:h-[50svw] lg:w-[40dvw] lg:h-[40dvw] p-4 text-white text-xl font-bebas-neue bg-black rounded-md'
+            className='lg:cursor-default relative w-full h-[50svh] md:w-[50svw] md:h-[50svw] lg:w-[40dvw] lg:h-[40dvw] p-4 text-white text-xl font-bebas-neue bg-black rounded-md overflow-hidden'
           >
             <div className='flex items-center justify-end w-full h-[10%]'>
               <h2 className='h-full w-1/3 center-elements'>Error</h2>
@@ -73,8 +73,37 @@ export default function DialogBackdrop({
                 </button>
               </div>
             </div>
+            <div className='w-full break-words h-[90%] flex items-center justify-start flex-col overflow-hidden'>
+              <p className='font-montserrat pb-4 w-full h-1/2 center-elements text-center overflow-auto'>
+                Sorry, but something went wrong. You can either close this
+                window and try again, or read the error details below, if
+                available.
+              </p>
+
+              <p
+                className={`w-full h-1/2 flex overflow-auto border-t pt-4 border-white ${
+                  errorDetails
+                    ? 'items-start justify-start '
+                    : 'items-center justify-center '
+                }`}
+              >
+                Error details:&nbsp;
+                {errorDetails
+                  ? `xxxxxvxxxxxvxxxxxvxxxxxvxxxx xxxxxxxxxxxxxxx
+                  xxxxx xxxxxxxxxx xxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxx
+                  xxxxxxxxxxxxxxxxxyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+                  xxxxxxxxxx xxxxxxx xxxxxv xxxxxvxxxxxv xxxxxvxxxxxv
+                  xxxxxvxxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxx
+                  xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+                  xxxxxxxxxxxxxxx
+                  xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+                  xxxxxxxxxxxxxxx xxxxxv xxxxxv xxxxxv xxxxxv
+                  xxxxxxxxxxxxxxxxxxxxv xxxxxv
+                  xxxxxvxxxxxvxxxxxvxxxxxvxxxxxvxxxxxvxxxxxvxxxxxvxxxxxvxv`
+                  : 'No error details found.'}
+              </p>
+            </div>
           </div>
-          <div />
         </dialog>
       </div>
     );
