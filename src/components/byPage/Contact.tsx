d364b4cfcc45478b806bff595e9b8e1773246d89
O. K. G. <inariwind@gmail.com>
2024-06-20 16:13:16 -0400

    fix: title

--- src/components/byPage/Contact.tsx
+++ src/components/byPage/Contact.tsx
@@ -2,14 +2,20 @@ import Section from '@components/shared/Section';
 import Title from '@components/title/Title';
 import ContactForm from '@components/contactForm/ContactForm';
 
+const MAIN_TITLE_GLOW_CLASSNAME = "before:content-['contact_me']";
+
 export default function Contact() {
   return (
     <Section
-      className='gap-2 sm:gap-0'
+      className='gap-8'
       backgroundClassName='bg-center'
       currentSection='contact'
     >
-      <Title label='contact' labelGlowText='contact' />
+      <Title
+        label='contact me'
+        border
+        labelGlowText={MAIN_TITLE_GLOW_CLASSNAME}
+      />
       <ContactForm />
     </Section>
   );
