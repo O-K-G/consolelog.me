84d47a4a7f51677b0afdd6a72f9f177b25487cea
O. K. G. <inariwind@gmail.com>
2024-06-21 17:14:59 -0400

    refactor: minor refactor

---
+++ src/constants/skillsLists.tsx
@@ -0,0 +1,6 @@
+export const SKILLS_LISTS = [
+  { column1: ['React.js', 'Next.js', 'Node.js', 'Express.js'] },
+  { column2: ['Tailwind CSS', 'Async JS', 'TypeScript', 'Zod'] },
+  { column3: ['Microservices', 'Agile methodology', 'REST API', 'MongoDB'] },
+  { column4: ['Responsive web design', 'i18n', 'A11y', 'Unit tests'] },
+] as const;
