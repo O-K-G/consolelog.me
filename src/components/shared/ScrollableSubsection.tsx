c4d6bc4a3c49e0c1da9b14ffa1c2e53fe3c40d8b
O. K. G. <inariwind@gmail.com>
2024-06-27 13:42:58 -0400

    refactor: rollback

--- src/components/shared/ScrollableSubsection.tsx
+++ src/components/shared/ScrollableSubsection.tsx
@@ -8,7 +8,7 @@ import {
   cloneElement,
   useState,
   type PropsWithChildren,
-  useEffect,
+  // useEffect,
 } from 'react';
 import useHandleHorizontalScroll from '@hooks/useHandleHorizontalScroll';
 
@@ -67,39 +67,40 @@ export default function ScrollableSubsection({
     }
   });
 
-  const { children: clds } = scrollableRef.current ?? { children: null };
+  // const { children: clds } = scrollableRef.current ?? { children: null };
 
-  useEffect(() => {
-    const options = {
-      rootMargin: '0px',
-      threshold: 1,
-    };
+  // useEffect(() => {
+  //   const options = {
+  //     rootMargin: '0px',
+  //     threshold: 0,
+  //   };
 
-    const handleObserve = (e: IntersectionObserverEntry[], id: number) => {
-      const { isIntersecting } = e[0];
+  //   const handleObserve = (e: IntersectionObserverEntry[], id: number) => {
+  //     const { isIntersecting } = e[0];
 
-      if (isIntersecting && id !== selectedSubsection) {
-        setSelectedSubsection(id);
-      }
-    };
+  //     if (isIntersecting && id !== selectedSubsection) {
+  //       console.log('test');
+  //       setSelectedSubsection(id);
+  //     }
+  //   };
 
-    if (clds) {
-      Object.values(clds).forEach((ch, index) => {
-        const { id } = childrenWithId?.[index]?.props as ChildrenWithIdProps;
-        const observer = new IntersectionObserver(
-          (e) => handleObserve(e, id),
-          options
-        );
-        observer.observe(ch as HTMLDivElement);
+  //   if (clds) {
+  //     Object.values(clds).forEach((ch, index) => {
+  //       const { id } = childrenWithId?.[index]?.props as ChildrenWithIdProps;
+  //       const observer = new IntersectionObserver(
+  //         (e) => handleObserve(e, id),
+  //         options
+  //       );
+  //       observer.observe(ch as HTMLDivElement);
 
-        return () => observer.disconnect();
-      });
-    }
+  //       return () => observer.disconnect();
+  //     });
+  //   }
 
-    if (selectedSubsection === null) {
-      setSelectedSubsection(0);
-    }
-  }, [childrenWithId, clds, selectedSubsection]);
+  //   if (selectedSubsection === null) {
+  //     setSelectedSubsection(0);
+  //   }
+  // }, [childrenWithId, clds, selectedSubsection]);
 
   return (
     <div className='z-10 size-full center-elements'>
