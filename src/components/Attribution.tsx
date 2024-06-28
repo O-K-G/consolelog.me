5165fb19c2b0dc0daf6baf0b96f519a312435cad
O. K. G. <inariwind@gmail.com>
2024-06-28 13:07:45 -0400

    feat: constants

--- src/components/Attribution.tsx
+++ src/components/Attribution.tsx
@@ -1,38 +1,42 @@
 import { useState } from 'react';
 import DialogBackdrop from '@components/shared/DialogBackdrop';
+import { useText } from '@hooks/useText';
+import attributionText from '@i18nEn/attributionText.json';
+import { URLs } from '@constants/urls';
 
 function AttributionContent() {
+  const t = useText();
+  const { figmaAttribution, ccbyLicense } = URLs;
+
   return (
     <p className='h-[90%] w-full break-words font-montserrat text-base sm:text-lg md:text-xl'>
-      Design based on Jayendra Awasthi&apos;s &apos;Space themed portfolio&apos;
-      free community Figma template at:
+      {t('designBasedOn', attributionText)}
       <a
         className='relative before:focus:absolute before:focus:size-full z-10 before:rounded-md before:-z-10 before:focus:bg-white transition-300 block mt-2 outline-none text-title-purple hover:text-[#75629f] active:text-black focus:text-[#75629f]'
         target='_blank'
         rel='noreferrer'
-        href='https://www.figma.com/community/file/1192903581929005722'
+        href={figmaAttribution}
       >
-        https://www.figma.com/community/file/1192903581929005722
+        {figmaAttribution}
       </a>
-      and used under the&nbsp;
+      {t('andUsedUnderThe', attributionText)}&nbsp;
       <a
         className='relative before:focus:absolute before:focus:size-full z-10 before:rounded-md before:-z-10 before:focus:bg-white transition-300 outline-none text-title-purple hover:text-[#75629f] active:text-black focus:text-[#75629f]'
         target='_blank'
         rel='noreferrer'
-        href='https://creativecommons.org/licenses/by/4.0/'
+        href={ccbyLicense}
       >
-        CC BY 4.0
+        {t('ccby', attributionText)}
       </a>
-      &nbsp; license.
-      <span className='block mt-2'>
-        Changes were made to the material from the original version.
-      </span>
+      &nbsp; {t('license', attributionText)}.
+      <span className='block mt-2'>{t('changes', attributionText)}.</span>
     </p>
   );
 }
 
 export default function Attribution() {
   const [open, setOpen] = useState(false);
+  const t = useText();
 
   return (
     <>
@@ -41,13 +45,13 @@ export default function Attribution() {
         className='mt-auto z-10 uppercase outline-none font-bebas-neue text-white hover:text-title-purple active:text-[#75629f] focus:text-title-purple text-base sm:text-xl md:text-4xl xl:text-6xl'
         onClick={() => setOpen(true)}
       >
-        Attribution
+        {t('title', attributionText)}
       </button>
 
       <DialogBackdrop
         open={open}
         onClose={() => setOpen(false)}
-        title='Attribution'
+        title={t('title', attributionText)}
         sizeClassName='w-full h-fit md:w-[50svw] lg:w-[40dvw]'
         contentSlot={<AttributionContent />}
       />
--- src/constants/urls.tsx
+++ src/constants/urls.tsx
@@ -1,4 +1,6 @@
 export const URLs = {
   gitHub: 'https://TBD',
   linkedIn: 'https://TBD',
+  figmaAttribution: 'https://www.figma.com/community/file/1192903581929005722',
+  ccbyLicense: 'https://creativecommons.org/licenses/by/4.0',
 } as const;
