706d7cb2443b070a87542bc0c6841229aec21c6b
O. K. G. <inariwind@gmail.com>
2024-06-30 21:21:58 -0400

    refactor: stage 3

--- src/components/shared/ScrollIconComponent.tsx
+++ src/components/shared/ScrollIconComponent.tsx
@@ -6,7 +6,10 @@ export default function ScrollIconComponent() {
   const t = useText();
 
   return (
-    <div className='fixed center-elements flex-col h-16 overflow-hidden md:h-[4.5rem] lg:h-24 bottom-0 right-0 mr-4 mb-4'>
+    <div
+      aria-hidden
+      className='fixed center-elements flex-col h-16 overflow-hidden md:h-[4.5rem] lg:h-24 bottom-0 right-0 mr-4 mb-4'
+    >
       <div className='uppercase text-title-purple text-base md:text-xl lg:text-2xl text-center w-full font-bebas-neue'>
         {t('scroll', scrollIconText)}
       </div>
