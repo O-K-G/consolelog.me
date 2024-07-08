bf0df46f3b48690066fb9d7559961360e819f973
O. K. G. <inariwind@gmail.com>
2024-07-08 17:31:12 -0400

    feat: favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata, Viewport } from 'next';
 import '@root/src/app/globals.css';
 import localFont from 'next/font/local';
 import { Bebas_Neue, Montserrat } from 'next/font/google';
+import { CACHE_VERSION } from '@root/tailwind.config';
 
 const bebasNeue = Bebas_Neue({
   weight: '400',
@@ -33,6 +34,22 @@ const starDate81316 = localFont({
 export const metadata: Metadata = {
   title: 'TBD',
   description: 'TBD',
+  applicationName: 'consolelogme',
+  keywords: ['TBD'],
+  manifest: '/manifest.json',
+  icons: {
+    icon: `/icon.png?cacheVersion=${CACHE_VERSION}`,
+    shortcut: `/shortcut-icon?cacheVersion=${CACHE_VERSION}`,
+    apple: `/apple-icon.png?cacheVersion=${CACHE_VERSION}`,
+    other: [
+      {
+        rel: 'mask-icon',
+        url: `/safari-pinned-tab.svg?cacheVersion=${CACHE_VERSION}`,
+        type: 'image/svg+xml',
+        sizes: 'any',
+      },
+    ],
+  },
 };
 
 export const viewport: Viewport = {
---
+++ src/app/manifest.ts
@@ -0,0 +1,26 @@
+import type { MetadataRoute } from 'next';
+import { CACHE_VERSION } from '@root/tailwind.config';
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'TBD',
+    short_name: 'TBD',
+    description: 'TBD',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#ffffff',
+    theme_color: '#ffffff',
+    icons: [
+      {
+        src: `/android-chrome-192x192.png?cacheVersion=${CACHE_VERSION}`,
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: `/android-chrome-512x512.png?cacheVersion=${CACHE_VERSION}`,
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  };
+}
