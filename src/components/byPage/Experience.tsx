0e8b1c95dd9c93d0ddec9dd20fcd4e1a3407fa4d
O. K. G. <inariwind@gmail.com>
2024-07-12 16:50:42 -0400

    fix: stage 1

--- src/components/byPage/Experience.tsx
+++ src/components/byPage/Experience.tsx
@@ -6,17 +6,16 @@ import { useText } from '@hooks/useText';
 import Attribution from '@components/Attribution';
 import ScrollableSubsection from '@components/shared/scrollableSection/ScrollableSubsection';
 import Subtitle from '@components/shared/Subtitle';
-import { ClickToOpenButton } from '@components/shared/ClickToOpenButton';
+// import { ClickToOpenButton } from '@components/shared/ClickToOpenButton';
 import experience from '@i18nEn/experience.json';
 import Shenanigans from '@components/Shenanigans';
 
 const CURRENT_SECTION = 'experience';
 const MAIN_TITLE_GLOW_CLASSNAME = "before:content-['my_work_experience']";
 const SUBTITLE_GLOW_CLASSNAME = "before:content-['some_place']";
-const SUBTITLE_CLASSNAME =
-  'absolute top-0 w-fit h-16 sm:h-24 md:h-[6.5rem] lg:h-28 xl:h-32 2xl:h-36';
-const OPEN_SIZE_CLASSNAME =
-  'data-[open=true]:w-3/5 data-[open=true]:md:w-4/5 data-[open=true]:h-1/2';
+
+// const OPEN_SIZE_CLASSNAME =
+//   'data-[open=true]:w-3/5 data-[open=true]:md:w-4/5 data-[open=true]:h-1/2';
 
 export default function Experience() {
   const t = useText();
@@ -30,41 +29,38 @@ export default function Experience() {
       <ScrollableSubsection>
         <ScrollableSubsection.Item>
           <Subtitle
-            className={SUBTITLE_CLASSNAME}
             label={t('subtitle', experience)}
             labelGlowText={SUBTITLE_GLOW_CLASSNAME}
           />
 
-          <ClickToOpenButton
+          {/* <ClickToOpenButton
             openSizeClassName={OPEN_SIZE_CLASSNAME}
             alternativeLabel={t('alternativeLabel', experience)}
-          />
+          /> */}
         </ScrollableSubsection.Item>
 
         <ScrollableSubsection.Item>
           <Subtitle
-            className={SUBTITLE_CLASSNAME}
             label={t('subtitle', experience)}
             labelGlowText={SUBTITLE_GLOW_CLASSNAME}
           />
 
-          <ClickToOpenButton
+          {/* <ClickToOpenButton
             openSizeClassName={OPEN_SIZE_CLASSNAME}
             alternativeLabel={t('alternativeLabel', experience)}
-          />
+          /> */}
         </ScrollableSubsection.Item>
 
         <ScrollableSubsection.Item>
           <Subtitle
-            className={SUBTITLE_CLASSNAME}
             label={t('subtitle', experience)}
             labelGlowText={SUBTITLE_GLOW_CLASSNAME}
           />
 
-          <ClickToOpenButton
+          {/* <ClickToOpenButton
             openSizeClassName={OPEN_SIZE_CLASSNAME}
             alternativeLabel={t('alternativeLabel', experience)}
-          />
+          /> */}
         </ScrollableSubsection.Item>
       </ScrollableSubsection>
 
