5d66cd094086f56fe3845d5652671661e4cc2ceb
O. K. G. <inariwind@gmail.com>
2024-07-13 12:59:13 -0400

    feat: stage 3

--- src/components/shared/ExpandableButton.tsx
+++ src/components/shared/ExpandableButton.tsx
@@ -1,9 +1,6 @@
 import { useState } from 'react';
 import AboutTargetIcon from '@components/shared/title/AboutTargetIcon';
 
-const OPEN_SIZE_CLASSNAME =
-  'data-[open=true]:w-full data-[open=true]:h-[100cqw] data-[open=true]:md:w-[100cqh] data-[open=true]:md:h-full data-[open=true]:border data-[open=true]:md:border-green-500';
-
 export default function ExpandableButton({
   label,
   alternativeLabel,
@@ -14,18 +11,15 @@ export default function ExpandableButton({
   const [open, setOpen] = useState(false);
 
   return (
-    <div className='z-10 container-type-size p-6 size-full relative max-h-[80%] center-elements border border-blue-500'>
+    <div className='z-10 container-type-size p-6 size-full relative max-h-[80%] center-elements'>
       <div
         data-open={open}
-        className={`transition-1000 group relative max-h-full center-elements w-1/2 h-[50cqw] lg:w-1/4 lg:h-[25cqw] ${OPEN_SIZE_CLASSNAME}`}
+        className='transition-1000 group relative max-h-full center-elements w-1/2 h-[50cqw] lg:w-1/4 lg:h-[25cqw] data-[open=true]:expandable-button-size-container'
       >
         <AboutTargetIcon open={open} />
         <button
-          className={`outline-none size-full ${
-            !open
-              ? 'text-sm sm:text-2xl md:text-4xl text-transparent title-text-stroke-white font-just-in-the-firestorm uppercase drop-shadow-[0px_0px_3px_rgba(175,25,255,1)]'
-              : ''
-          }`}
+          data-open={open}
+          className='transition-1000 outline-none overflow-hidden size-full data-[open=false]:closed-expandable-button data-[open=true]:opened-expandable-button'
           type='button'
           onClick={() => setOpen((prevValue) => !prevValue)}
         >
