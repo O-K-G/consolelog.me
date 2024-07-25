3d7b4147c7ea3d03289c091f38398ad161ffc255
O. K. G. <inariwind@gmail.com>
2024-07-25 14:49:44 -0400

    fix: stage 1

--- src/components/shared/expandableButton/AboutTargetIcon.tsx
+++ src/components/shared/expandableButton/AboutTargetIcon.tsx
@@ -27,19 +27,25 @@ export default function AboutTargetIcon({
     : '-bottom-5 -right-5 rotate-180';
 
   return (
-    <>
-      <SVG90Degrees
-        className={`${!open ? '' : 'opacity-0'} ${className}`}
-        pathClassName='stroke-title-purple blur-[0.125rem] group-hover:stroke-title-orange group-active:stroke-title-red'
-      />
-      <SVG90Degrees
-        className={className}
-        pathClassName={`${
-          !open
-            ? 'stroke-white group-hover:stroke-title-orange group-active:stroke-title-red'
-            : 'stroke-title-purple group-hover:stroke-title-orange group-active:stroke-title-red'
-        }`}
-      />
-    </>
+    <div className={`absolute ${className}`}>
+      <div className='before:bg-white before:w-1 before:h-2 before:absolute before:-top-2 before:right-0 border-double border-t-8 border-l-8 border-white size-24 relative after:bg-white after:w-2 after:h-1 after:absolute after:bottom-0 after:-left-2' />
+    </div>
   );
+
+  // return (
+  //   <>
+  //     <SVG90Degrees
+  //       className={`${!open ? '' : 'opacity-0'} ${className}`}
+  //       pathClassName='stroke-title-purple blur-[0.125rem] group-hover:stroke-title-orange group-active:stroke-title-red'
+  //     />
+  //     <SVG90Degrees
+  //       className={className}
+  //       pathClassName={`${
+  //         !open
+  //           ? 'stroke-white group-hover:stroke-title-orange group-active:stroke-title-red'
+  //           : 'stroke-title-purple group-hover:stroke-title-orange group-active:stroke-title-red'
+  //       }`}
+  //     />
+  //   </>
+  // );
 }
