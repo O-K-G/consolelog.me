cd205ea7248ecffeb63c4893110c0813304765e0
O. K. G. <inariwind@gmail.com>
2024-07-25 15:32:05 -0400

    fix: stage 6

--- src/components/shared/expandableButton/AboutTargetIcon.tsx
+++ src/components/shared/expandableButton/AboutTargetIcon.tsx
@@ -9,7 +9,7 @@ export default function AboutTargetIcon({ bottom }: AboutTargetIconProps) {
     <div
       className={`group absolute overflow-hidden drop-shadow-border-purple-glow w-[1.9185rem] h-[1.702rem] md:w-[3.837rem] md:h-[3.404rem] ${className}`}
     >
-      <div className='transition-1000 before:transition-1000 after:transition-1000 group-hover:before:bg-orange-300 group-active:before:bg-red-400 before:bg-white before:w-1 before:h-2 before:absolute before:-top-2 before:right-0 border-double border-t-4 border-l-4 md:border-t-8 md:border-l-8 border-white group-hover:border-orange-300 group-active:border-red-400 size-full relative group-hover:after:bg-orange-300 group-active:after:bg-red-400 after:bg-white after:w-2 after:h-1 after:absolute after:bottom-0 after:-left-2' />
+      <div className='transition-1000 before:transition-1000 after:transition-1000 group-hover:before:bg-orange-300 group-active:before:bg-red-400 before:bg-white before:w-0.5 before:md:w-1 before:h-2 before:absolute before:-top-2 before:right-0 border-double border-t-[6px] border-l-[6px] md:border-t-8 md:border-l-8 border-white group-hover:border-orange-300 group-active:border-red-400 size-full relative group-hover:after:bg-orange-300 group-active:after:bg-red-400 after:bg-white after:w-2 after:h-0.5 after:md:h-1 after:absolute after:bottom-0 after:-left-2' />
     </div>
   );
 }
