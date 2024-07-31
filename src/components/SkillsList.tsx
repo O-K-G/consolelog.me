f7fea40a26836a02595f65cea2e78b583a0dfd2f
O. K. G. <inariwind@gmail.com>
2024-07-31 15:47:54 -0400

    fix: minor a11y fix

--- src/components/SkillsList.tsx
+++ src/components/SkillsList.tsx
@@ -2,25 +2,25 @@ import { SKILLS_LISTS } from '@constants/skillsLists';
 
 function Column({ children }: { children: any }) {
   return (
-    <div className='w-[45%] max-w-[10.047rem] sm:max-w-[auto] sm:w-[10.047rem] lg:w-[13.396rem] flex justify-start items-center flex-col gap-y-4 z-10 min-w-1/4'>
+    <ul className='w-[45%] max-w-[10.047rem] sm:max-w-[auto] sm:w-[10.047rem] lg:w-[13.396rem] flex justify-start items-center flex-col gap-y-4 z-10 min-w-1/4'>
       {children}
-    </div>
+    </ul>
   );
 }
 
 function SkillBlock({ str }: { str: string }) {
   return (
-    <div className='transition-300 bg-black/30 hover:bg-black/70 w-full h-[4.92225rem] lg:h-[6.563rem] p-1 shrink-0 center-elements overflow-hidden border-2 border-title-purple'>
-      <li className='size-full break-words text-center center-elements text-white font-montserrat text-sm sm:text-base md:text-xl'>
+    <li className='transition-300 bg-black/30 hover:bg-black/70 w-full h-[4.92225rem] lg:h-[6.563rem] p-1 shrink-0 center-elements overflow-hidden border-2 border-title-purple'>
+      <div className='size-full break-words text-center center-elements text-white font-montserrat text-sm sm:text-base md:text-xl'>
         {str}
-      </li>
-    </div>
+      </div>
+    </li>
   );
 }
 
 export default function SkillsList() {
   return (
-    <ul className='flex items-start justify-center flex-wrap p-2.5 gap-4 h-full'>
+    <div className='flex items-start justify-center flex-wrap p-2.5 gap-4 h-full'>
       {SKILLS_LISTS.map((obj) => {
         const col = Object.keys(obj)[0];
 
@@ -32,6 +32,6 @@ export default function SkillsList() {
           </Column>
         );
       })}
-    </ul>
+    </div>
   );
 }
