4b0ebe7243a0b556881210e5e92deb64a124ead1
O. K. G. <inariwind@gmail.com>
2024-08-02 19:42:08 -0400

    refactor: minor refactor

--- src/components/shared/SectionBackground.tsx
+++ src/components/shared/SectionBackground.tsx
@@ -33,7 +33,7 @@ export default function SectionBackground({ currentSection }: CurrentSection) {
           className='object-cover object-left h-screen w-screen bottom-0 left-0 fixed opacity-60'
           alt=''
           aria-hidden
-          src={`${currentSection}-2xl${SUFFIX}`}
+          src={`images/${currentSection}-2xl${SUFFIX}`}
         />
       </picture>
     </div>
