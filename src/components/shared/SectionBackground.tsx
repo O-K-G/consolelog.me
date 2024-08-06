c7f5a4bcdcc82ee4ae493c8a971ad32c617efcaa
O. K. G. <inariwind@gmail.com>
2024-08-05 21:57:00 -0400

    feat: planet image sizes

--- src/components/shared/SectionBackground.tsx
+++ src/components/shared/SectionBackground.tsx
@@ -1,18 +1,12 @@
 import type { CurrentSection } from '@constants/interfaces';
 import { CACHE_VERSION } from '@root/tailwind.config';
+import { TAILWIND_SIZES } from '@constants/imagesConfig';
 
 /** This component is a fix to cases in mobile devices in which the user scrolls down in "extreme velocity",
  * and once the browser hits the bottom - the background flickers for a fraction of a second.
  */
 
 const SUFFIX = `.webp?cacheVersion=${CACHE_VERSION}`;
-const BACKGROUND_SIZES = [
-  { twClassName: 'sm', pxResolution: 640 },
-  { twClassName: 'md', pxResolution: 768 },
-  { twClassName: 'lg', pxResolution: 1024 },
-  { twClassName: 'xl', pxResolution: 1280 },
-  { twClassName: '2xl', pxResolution: 1536 },
-];
 
 export default function SectionBackground({ currentSection }: CurrentSection) {
   if (!currentSection) {
@@ -22,7 +16,7 @@ export default function SectionBackground({ currentSection }: CurrentSection) {
   return (
     <div className='absolute top-0 left-0 min-h-full h-screen w-screen clip-path-inset-0'>
       <picture>
-        {BACKGROUND_SIZES.map(({ twClassName, pxResolution }) => (
+        {TAILWIND_SIZES.map(({ twClassName, pxResolution }) => (
           <source
             key={`tw-class-${twClassName}`}
             srcSet={`/images/${currentSection}-${twClassName}${SUFFIX}`}
---
+++ src/constants/imagesConfig.tsx
@@ -0,0 +1,7 @@
+export const TAILWIND_SIZES = [
+  { twClassName: 'sm', pxResolution: 640 },
+  { twClassName: 'md', pxResolution: 768 },
+  { twClassName: 'lg', pxResolution: 1024 },
+  { twClassName: 'xl', pxResolution: 1280 },
+  { twClassName: '2xl', pxResolution: 1536 },
+] as const;
