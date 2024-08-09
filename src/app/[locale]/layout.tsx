93ecdc09d4b2914dee2bd61f9e0c3c62952c0b0e
O. K. G. <inariwind@gmail.com>
2024-08-09 19:12:33 -0400

    fix: minor fix

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -59,7 +59,7 @@ export default async function RootLayout({
     <html dir={dir} lang={selectedLocale}>
       <NextIntlClientProvider messages={messages}>
         <body
-          className={`has-[dialog]:overflow-hidden has-[main_aside[data-open=true]]:overflow-hidden ${mainTitle} ${borderTitle} ${infoText} ${regularText}`}
+          className={`has-[dialog[data-open=true]]:overflow-hidden has-[main_aside[data-open=true]]:overflow-hidden ${mainTitle} ${borderTitle} ${infoText} ${regularText}`}
         >
           {children}
         </body>
