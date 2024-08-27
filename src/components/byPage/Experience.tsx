eed0bda56a9330d27cd965f614f30964e1de0a7c
O. K. G. <inariwind@gmail.com>
2024-08-27 19:19:31 -0400

    feat: content - 5

--- src/components/byPage/Experience.tsx
+++ src/components/byPage/Experience.tsx
@@ -14,24 +14,14 @@ export default function Experience() {
 
   return (
     <Section
-      className='min-h-[55rem] relative h-svh lg:h-dvh pt-20 md:pt-40'
+      className='min-h-[100svh] lg:min-h-[100dvh] relative h-svh lg:h-dvh pt-20 md:pt-40'
       currentSection={CURRENT_SECTION}
     >
       <Title border>{t('mainTitle')}</Title>
       <ScrollableSubsection>
         <ScrollableSubsection.Item>
-          <Title variant='subtitle'>{t('subtitle')}</Title>
-          <ExpandableButton alternativeLabel={t('alternativeLabel')} />
-        </ScrollableSubsection.Item>
-
-        <ScrollableSubsection.Item>
-          <Title variant='subtitle'>{t('subtitle')}</Title>
-          <ExpandableButton alternativeLabel={t('alternativeLabel')} />
-        </ScrollableSubsection.Item>
-
-        <ScrollableSubsection.Item>
-          <Title variant='subtitle'>{t('subtitle')}</Title>
-          <ExpandableButton alternativeLabel={t('alternativeLabel')} />
+          <Title variant='subtitle'>{t('bankMainTitle')}</Title>
+          <ExpandableButton alternativeLabel={t('bankMainTitleDescription')} />
         </ScrollableSubsection.Item>
       </ScrollableSubsection>
       <Footer />
