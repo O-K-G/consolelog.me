36a8555322f6e0106dcce8e461677ccc1958e869
O. K. G. <inariwind@gmail.com>
2024-09-02 22:25:18 -0400

    fix: expandable button scroll y on firefox

--- src/components/shared/expandableButton/ExpandableButton.tsx
+++ src/components/shared/expandableButton/ExpandableButton.tsx
@@ -21,6 +21,15 @@ export default function ExpandableButton({
     );
   };
 
+  const handleClick = () => {
+    setOpen((prevValue) => !prevValue);
+    setAnimationEnd(false);
+    (buttonRef.current as unknown as HTMLButtonElement).addEventListener(
+      'animationend',
+      handleTextAnimation
+    );
+  };
+
   return (
     <div className='z-10 container-type-size p-6 h-full w-full sm:w-9/12 md:w-10/12 relative max-h-[80%] center-elements'>
       <div
@@ -28,27 +37,23 @@ export default function ExpandableButton({
         className='transition-1000 group relative max-h-full center-elements w-1/2 h-[50cqw] lg:w-1/4 lg:h-[25cqw] data-[open=true]:expandable-button-size-container'
       >
         <AboutTargetIcon open={open} />
-        <button
+        <div
+          role='button'
+          tabIndex={0}
           ref={buttonRef}
           aria-label={!open ? t('clickToOpen') : alternativeLabel}
           aria-expanded={open}
           data-open={open}
-          className={`transition-300 border-x border-transparent outline-none overflow-x-hidden data-[open=false]:overflow-y-hidden data-[open=true]:overflow-y-auto size-full data-[open=false]:closed-expandable-button data-[open=true]:opened-expandable-button data-[open=true]:regular-text-font-by-locale ${
+          className={`transition-300 center-elements text-center border-x border-transparent outline-none overflow-x-hidden data-[open=false]:overflow-y-hidden data-[open=true]:overflow-y-auto size-full data-[open=false]:closed-expandable-button data-[open=true]:opened-expandable-button data-[open=true]:regular-text-font-by-locale ${
             !isAnimationEnd
               ? ''
               : 'data-[open=true]:border-white data-[open=false]:closed-expandable-button-focus'
           }`}
-          type='button'
-          onClick={() => {
-            setOpen((prevValue) => !prevValue);
-            setAnimationEnd(false);
-            (
-              buttonRef.current as unknown as HTMLButtonElement
-            ).addEventListener('animationend', handleTextAnimation);
-          }}
+          onClick={handleClick}
+          onKeyDown={handleClick}
         >
           {isAnimationEnd && (!open ? t('clickToOpen') : alternativeLabel)}
-        </button>
+        </div>
         <AboutTargetIcon bottom open={open} />
       </div>
     </div>
