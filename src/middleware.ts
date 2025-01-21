e6c40797778e9ce9e4a2bc157a2392b1c6558b8e
O-K-G <inariwind@gmail.com>
2025-01-21 00:20:21 -0500

    refactor: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,13 @@
-import createIntlMiddleware from 'next-intl/middleware';
-import { type NextRequest, NextResponse } from 'next/server';
-
-const intlMiddleware = createIntlMiddleware({
-  locales: ['en', 'he'],
-  defaultLocale: 'en',
-});
+import createMiddleware from 'next-intl/middleware';
+import { routing } from '@i18n/routing';
+import { NextRequest, NextResponse } from 'next/server';
 
 const ALLOWED_DOMAINS =
   '*.vercel.live vercel.live *.vercel.com vercel.com *.pusher.com pusher.com wss://*.pusher.com wss://pusher.com';
 
-export async function middleware(request: NextRequest) {
-  const intlResponse = intlMiddleware(request);
+const intlMiddleware = createMiddleware(routing);
 
+export default function middleware(request: NextRequest) {
   const nonce = Buffer.from(crypto.randomUUID()).toString('base64');
 
   const isDevEnv = process.env.NODE_ENV === 'development';
@@ -31,53 +27,42 @@ export async function middleware(request: NextRequest) {
 
   // TODO: Check style-src 'self' ${prevEnv};
 
-  // const cspHeader = `
-  //   default-src 'self';
-  //   script-src 'self' 'unsafe-inline' http: https: 'nonce-${nonce}' ${
-  //   isPrevEnv ? '' : 'strict-dynamic'
-  // } ${devEnv} ${noNoncePrevEnv};
-  //   style-src 'self' 'unsafe-inline';
-  //   img-src 'self' blob: data: ${noNoncePrevEnv};
-  //   font-src 'self';
-  //   connect-src 'self' ${prevEnv};
-  //   frame-src 'self' ${prevEnv};
-  //   object-src 'none';
-  //   base-uri 'self';
-  //   form-action 'self';
-  //   frame-ancestors 'none';
-  //   ${!isDevEnv ? 'upgrade-insecure-requests;' : ''}
-  // `;
-
-  const cspHeader = '';
-  const contentSecurityPolicyHeaderValue = cspHeader
+  const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-inline' http: https: 'nonce-${nonce}' ${
+    isPrevEnv ? '' : 'strict-dynamic'
+  } ${devEnv} ${noNoncePrevEnv};
+    style-src 'self' ${ALLOWED_DOMAINS} 'unsafe-inline';
+    img-src 'self' blob: data: ${noNoncePrevEnv};
+    font-src 'self';
+    connect-src 'self' ${prevEnv};
+    frame-src 'self' ${prevEnv};
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    ${!isDevEnv ? 'upgrade-insecure-requests;' : ''}
+  `
     .replace(/\s{2,}/g, ' ')
     .trim();
 
-  const response = intlResponse || NextResponse.next();
-  response.headers.set('x-nonce', nonce);
-  response.headers.set('X-Content-Type-Options', 'nosniff');
-  response.headers.set('Referrer-Policy', 'no-referrer');
-  response.headers.set(
-    'Permissions-Policy',
-    'camera=(), microphone=(), geolocation=()'
-  );
-  response.headers.set(
-    'Content-Security-Policy',
-    contentSecurityPolicyHeaderValue
-  );
+  const cspWithNonce = cspHeader;
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set('x-nonce', nonce);
+  requestHeaders.set('Content-Security-Policy', cspWithNonce);
+
+  const req = new NextRequest(request, {
+    headers: requestHeaders,
+  });
+
+  const response: NextResponse = intlMiddleware(req);
+  response.headers.set('Content-Security-Policy', cspWithNonce);
 
   return response;
 }
 
 export const config = {
   matcher: [
-    {
-      source:
-        '/((?!api|_next/static|_next/image|images|favicon.ico|icon.ico|apple-icon.png|manifest.webmanifest).*)',
-      missing: [
-        { type: 'header', key: 'next-router-prefetch' },
-        { type: 'header', key: 'purpose', value: 'prefetch' },
-      ],
-    },
+    '/((?!api|_next/static|_next/image|images|favicon.ico|icon.ico|apple-icon.png|manifest.webmanifest).*)',
   ],
 };
