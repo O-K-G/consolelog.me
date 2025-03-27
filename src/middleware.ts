1d4831f50ccc5a6c67500c7b30ee0d2c6ad04be6
O-K-G <inariwind@gmail.com>
2025-03-27 16:36:50 -0400

    refactor: bump

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,25 @@
-import createMiddleware from 'next-intl/middleware';
-import { routing } from '@i18n/routing';
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
+import createMiddleware from "next-intl/middleware";
+import { routing } from "./i18n/routing";
 
 const ALLOWED_DOMAINS =
-  '*.vercel.live vercel.live *.vercel.com vercel.com *.pusher.com pusher.com wss://*.pusher.com wss://pusher.com';
+  "*.vercel.live vercel.live *.vercel.com vercel.com *.pusher.com pusher.com wss://*.pusher.com wss://pusher.com";
 
 const intlMiddleware = createMiddleware(routing);
 
 export default function middleware(request: NextRequest) {
-  const nonce = Buffer.from(crypto.randomUUID()).toString('base64');
+  const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
 
-  const isDevEnv = process.env.NODE_ENV === 'development';
-  const isPrevEnv = process.env?.VERCEL_ENV === 'preview';
+  const isDevEnv = process.env.NODE_ENV === "development";
+  const isPrevEnv = process.env?.VERCEL_ENV === "preview";
 
-  const devEnv = isDevEnv ? "'unsafe-eval'" : '';
+  const devEnv = isDevEnv ? "'unsafe-eval'" : "";
 
   const prevEnv = isPrevEnv
     ? `${ALLOWED_DOMAINS} 'unsafe-inline'`
     : `'nonce-${nonce}'`;
 
-  const noNoncePrevEnv = isPrevEnv ? `${ALLOWED_DOMAINS} 'unsafe-inline'` : '';
+  const noNoncePrevEnv = isPrevEnv ? `${ALLOWED_DOMAINS} 'unsafe-inline'` : "";
 
   /** Backwards compatibility at script-src: 'unsafe inline' is ignored if there's a nonce or hash in CSP2+.
    * It's also ignored in CSP3 if there's 'strict-dynamic'.
@@ -30,7 +30,7 @@ export default function middleware(request: NextRequest) {
   const cspHeader = `
     default-src 'self';
     script-src 'self' 'unsafe-inline' http: https: 'nonce-${nonce}' ${
-    isPrevEnv ? '' : 'strict-dynamic'
+    isPrevEnv ? "" : "strict-dynamic"
   } ${devEnv} ${noNoncePrevEnv};
     style-src 'self' ${ALLOWED_DOMAINS} 'unsafe-inline';
     img-src 'self' blob: data: ${noNoncePrevEnv};
@@ -41,28 +41,28 @@ export default function middleware(request: NextRequest) {
     base-uri 'self';
     form-action 'self';
     frame-ancestors 'none';
-    ${!isDevEnv ? 'upgrade-insecure-requests;' : ''}
+    ${!isDevEnv ? "upgrade-insecure-requests;" : ""}
   `
-    .replace(/\s{2,}/g, ' ')
+    .replace(/\s{2,}/g, " ")
     .trim();
 
   const cspWithNonce = cspHeader;
   const requestHeaders = new Headers(request.headers);
-  requestHeaders.set('x-nonce', nonce);
-  requestHeaders.set('Content-Security-Policy', cspWithNonce);
+  requestHeaders.set("x-nonce", nonce);
+  requestHeaders.set("Content-Security-Policy", cspWithNonce);
 
   const req = new NextRequest(request, {
     headers: requestHeaders,
   });
 
   const response: NextResponse = intlMiddleware(req);
-  response.headers.set('Content-Security-Policy', cspWithNonce);
+  response.headers.set("Content-Security-Policy", cspWithNonce);
 
   return response;
 }
 
 export const config = {
   matcher: [
-    '/((?!api|_next/static|_next/image|images|favicon.ico|icon.ico|apple-icon.png|manifest.webmanifest).*)',
+    "/((?!api|_next/static|_next/image|images|favicon.ico|icon.ico|apple-icon.png|manifest.webmanifest).*)",
   ],
 };
